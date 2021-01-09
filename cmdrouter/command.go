de41b27e9fd2755bccd0439689ac23d0e993bf57
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-10 07:14:18 +0900

    make Command

---
+++ cmdrouter/command.go
@@ -0,0 +1,12 @@
+package cmdrouter
+
+// Command holds data about the command
+type Command struct {
+	Name        string
+	Alias       string
+	Summary     string
+	Usage       string
+	Example     string
+	SubCommands []*Command
+	Handler     *CommandHandler
+}
