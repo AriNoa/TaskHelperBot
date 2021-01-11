1120959f90236067324c8c54b514ef8c87c45062
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-12 03:55:38 +0900

    add DetachCommandFrom test

---
+++ cmdrouter/util.go
@@ -0,0 +1,6 @@
+package cmdrouter
+
+// DetachCommandFrom function detaches the command from the string and returns it and its arguments
+func DetachCommandFrom(str string) (command string, argument string) {
+	return "XXX", "XXX"
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module TaskHelperBot-Go
 
 go 1.15
 
-require github.com/bwmarrin/discordgo v0.22.0
+require (
+	github.com/bwmarrin/discordgo v0.22.0
+	github.com/stretchr/testify v1.6.1
+)
