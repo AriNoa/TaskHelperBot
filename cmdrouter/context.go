8d641a07038c2b5103e5ebb03c5b85842f33353d
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-13 13:37:51 +0900

    update removed dependency on discordgo

--- cmdrouter/context.go
+++ cmdrouter/context.go
@@ -1,11 +1,7 @@
 package cmdrouter
 
-import "github.com/bwmarrin/discordgo"
-
 // Context is the Context passed to the command handler
 type Context struct {
-	Session  discordgo.Session
-	Event    discordgo.MessageCreate
 	Argument string
 	Props    map[string]interface{}
 }
