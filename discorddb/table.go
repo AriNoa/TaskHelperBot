efb523a1d61a35f306ca2b4d3cc21066fbdaad2b
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-19 20:45:03 +0900

    make Table

---
+++ discorddb/table.go
@@ -0,0 +1,23 @@
+package discorddb
+
+import "encoding/json"
+
+// Table holds the data marshalled to JSON and the corresponding Discord message
+type Table struct {
+	Value     []byte
+	MessageID string
+}
+
+// NewTable is a constructor for Router
+func (t *Table) NewTable(value interface{}, messageID string) (*Table, error) {
+	json, err := json.Marshal((value))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Table{
+		json,
+		messageID,
+	}, nil
+}
