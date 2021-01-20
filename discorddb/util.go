d2fcd98b80a332f5293d64d5155bc04038f93e52
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-20 19:17:26 +0900

    make CreateTableFromMessage function

--- discorddb/util.go
+++ discorddb/util.go
@@ -1,6 +1,8 @@
 package discorddb
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
 
 	"github.com/bwmarrin/discordgo"
@@ -17,4 +19,37 @@ func DeleteMessageWithLog(s *discordgo.Session, channelID string, messageID stri
 	log.Printf("Delete message \"[%s]%s\"", m.Author.Username, m.Content)
 
 	return s.ChannelMessageDelete(channelID, messageID)
+}
+
+// CreateTableFromMessage creates a table from the message and returns the key and table
+func CreateTableFromMessage(s *discordgo.Session, m *discordgo.Message) (string, *Table, error) {
+	var hash map[string]interface{}
+
+	err := json.Unmarshal([]byte(m.Content), &hash)
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	_key, ok := hash["key"]
+	if !ok {
+		return "", nil, errors.New("message does not have \"key\"")
+	}
+
+	key, ok := _key.(string)
+	if !ok {
+		return "", nil, errors.New("key type is not string")
+	}
+
+	value, ok := hash["value"]
+	if !ok {
+		return "", nil, errors.New("message does not have \"value\"")
+	}
+
+	t, err := NewTable(value, m.ID)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return key, t, nil
 }
