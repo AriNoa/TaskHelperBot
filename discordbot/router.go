a9ff8b8f7cc0bd85265e9075889e9e389df3b720
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-29 15:48:06 +0900

    add HelpPresenter

--- discordbot/router.go
+++ discordbot/router.go
@@ -7,26 +7,37 @@ import (
 	"TaskHelperBot/domain/usecase/echo"
 )
 
-func newRouter() *cmdr.Router {
+func newEchoCommand() *cmdr.Command {
 	echoPresenter := &commands.EchoPresenter{}
+	var echoHandler commands.EchoHandler
 
+	echoCommand := cmdr.NewCommand(
+		"Echo",
+		"echo",
+		"任意の文字列を表示することができます",
+		"echo [文字列]",
+		"echo Hello world",
+		[]*cmdr.Command{},
+		&echoHandler,
+	)
+
+	echoHandler = commands.EchoHandler{
+		Command: echoCommand,
+		UseCase: &echo.Interactor{
+			Presenter: echoPresenter,
+		},
+		Presenter:     echoPresenter,
+		HelpPresenter: &commands.HelpPresenter{},
+	}
+
+	return echoCommand
+}
+
+func newRouter() *cmdr.Router {
 	return cmdr.NewRouter(
 		"!",
 		[]*cmdr.Command{
-			cmdr.NewCommand(
-				"Echo",
-				"echo",
-				"任意の文字列を表示することができます",
-				"echo [文字列]",
-				"echo Hello world",
-				[]*cmdr.Command{},
-				&commands.EchoHandler{
-					UseCase: &echo.Interactor{
-						Presenter: echoPresenter,
-					},
-					Presenter: echoPresenter,
-				},
-			),
+			newEchoCommand(),
 		},
 	)
 }
