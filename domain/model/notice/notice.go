180e09ad401b453ce0aac7b94a1445393f578fb5
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-31 15:16:44 +0900

    make constructor

--- domain/model/notice/notice.go
+++ domain/model/notice/notice.go
@@ -17,6 +17,33 @@ type Notice struct {
 	Enabled bool
 }
 
+// New is a constructor for Notice
+func New(
+	id int,
+	contnts string,
+	next time.Time,
+	interval time.Duration,
+	enabled bool,
+) *Notice {
+	var repeat struct {
+		value    time.Duration
+		hasValue bool
+	}
+
+	if interval != 0 {
+		repeat.hasValue = true
+		repeat.value = interval
+	}
+
+	return &Notice{
+		id,
+		contnts,
+		next,
+		repeat,
+		enabled,
+	}
+}
+
 // UpdateNextNoticeTime updates the next notification time from the Repeat value
 func (n *Notice) UpdateNextNoticeTime() error {
 	if !n.Repeat.hasValue {
