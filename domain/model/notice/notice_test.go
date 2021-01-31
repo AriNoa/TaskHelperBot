943edb0de6a2cdfeadbef521ed7a87ab05e91fe0
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-31 15:35:47 +0900

    add test case

--- domain/model/notice/notice_test.go
+++ domain/model/notice/notice_test.go
@@ -1,6 +1,7 @@
 package notice
 
 import (
+	"errors"
 	"log"
 	"testing"
 	"time"
@@ -24,4 +25,18 @@ func TestUpdateNextNoticeTime(t *testing.T) {
 	}
 
 	assert.Equal(t, n.Next, time.Date(2021, time.January, 1, 1, 46, 24, 0, time.UTC))
+}
+
+func TestUpdateNextNoticeTimeError(t *testing.T) {
+	n := New(
+		0,
+		"test",
+		time.Date(2020, time.December, 31, 22, 42, 24, 0, time.UTC),
+		0,
+		true,
+	)
+
+	err := n.UpdateNextNoticeTime()
+
+	assert.Equal(t, err, errors.New("Notice's repeat has not value"))
 }
