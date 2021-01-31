ec6de747798f2fc6605562a865713c48ae3dac7a
AriNoa <ikasumi.nekoneko@gmail.com>
2021-01-31 15:30:20 +0900

    make notice test

---
+++ domain/model/notice/notice_test.go
@@ -0,0 +1,27 @@
+package notice
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateNextNoticeTime(t *testing.T) {
+	n := New(
+		0,
+		"test",
+		time.Date(2020, time.December, 31, 22, 42, 24, 0, time.UTC),
+		3*time.Hour+4*time.Minute,
+		true,
+	)
+
+	err := n.UpdateNextNoticeTime()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, n.Next, time.Date(2021, time.January, 1, 1, 46, 24, 0, time.UTC))
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
