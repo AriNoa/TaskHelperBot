dd589fc9d22f62d6eef64e29b23167b65cb94e7b
AriNoa <ikasumi.nekoneko@gmail.com>
2021-02-01 12:15:34 +0900

    make constructors

--- domain/model/notice/structs.go
+++ domain/model/notice/structs.go
@@ -6,8 +6,36 @@ type Table struct {
 	Notices map[int]*Notice
 }
 
+// NewTable is a constructor for Table
+func NewTable(id string, notices []*Notice) *Table {
+	hash := map[int]*Notice{}
+
+	for _, n := range notices {
+		hash[n.ID] = n
+	}
+
+	return &Table{
+		id,
+		hash,
+	}
+}
+
 // User is a struct that represents a collection of user's Tables
 type User struct {
 	ID     string
 	Tables map[string]*Table
+}
+
+// NewUser is a constructor for User
+func NewUser(id string, tables []*Table) *User {
+	hash := map[string]*Table{}
+
+	for _, t := range tables {
+		hash[t.ID] = t
+	}
+
+	return &User{
+		id,
+		hash,
+	}
 }
